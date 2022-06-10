713f9a11848f62ec5f27962b6886c72af8b50389
S-ngase <achalasamuel2000@gmail.com>
2022-06-10 07:13:13 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,10 +6,13 @@
 void print_line(int n)
 {
 	int x;
-
+	int n = -4;
 	if (n <= 0)
 		_putchar('\n');
-	for (x = 0; x <= n; x++)
-		_putchar(95);
-	_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		    _putchar(95);
+		_putchar('\n');
+	}
 }
